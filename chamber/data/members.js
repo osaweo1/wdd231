ad564d42a77b11971934097e44b0b329ee8703ed
osawe osamudiamen <osamudiamenosawe@gmail.com>
2026-03-16 01:47:24 +0100

    chambers site

---
+++ chamber/data/members.js
@@ -0,0 +1,51 @@
+[
+{
+"name":"Palavy Tailoring ",
+"address":"Apkapkava, Benin City",
+"phone":"+234 807 555 6466",
+"website":"https://thepalavy.com",
+"image":"https://img.freepik.com/premium-vector/illustration-tailor-sewing-clothes_207579-2039.jpg",
+"membership":5,
+"category":"Services"
+}
+,
+{
+"name":"Telso Tech Solutions",
+"address":"12 Akpakpava Road, Benin City",
+"phone":"+234 801 234 5678",
+"website":"https://benintech.com",
+"image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtVfItC4ZEArd4Romr670eYQRaQCZfBV3Z2w&s",
+"membership":4,
+"category":"Technology"
+},
+
+{
+"name":"Royal Heritage Hotel",
+"address":"Airport Road, Benin City",
+"phone":"+234 802 111 2222",
+"website":"https://royalheritagehotel.com",
+"image":"https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2c/0c/e3/03/hotel-royal-heritage.jpg?w=900&h=500&s=1",
+"membership":3,
+"category":"Hospitality"
+},
+
+{
+"name":"Edo Agro Farms",
+"address":"Sapele Road, Benin City",
+"phone":"+234 803 555 7777",
+"website":"https://edoagro.com",
+"image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2kHXU_cLaWHpCh1W8x9MSJsN__flpTkG5kg&s",
+"membership":2,
+"category":"Agriculture"
+},
+
+{
+"name":"Oredo Printing Press",
+"address":"Ring Road, Benin City",
+"phone":"+234 807 555 6666",
+"website":"https://oredoprint.com",
+"image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWnfJWA0ViW-N02RrOGlpU5skbbfP3cCBrXg&s",
+"membership":1,
+"category":"Services"
+}
+]
---
+++ chamber/scripts/directory.js
@@ -0,0 +1,60 @@
+const searchInput=document.querySelector("#searchInput")
+const searchCategory=document.querySelector("#categoryFilter")
+const memberSection=document.querySelector("#members")
+
+const url="data/members.js"
+console.log(url)
+
+let members=[]
+
+
+const getMembers=async()=>{
+    const response=await fetch(url)
+    members=await response.json()
+    displayMembers(members)
+}
+
+const displayMembers=async(membersList)=>{
+    memberSection.innerHTML=""
+    membersList.forEach(member=>{
+
+        const card = document.createElement("section")
+        card.classList.add("card")
+
+        card.innerHTML = `
+        <img src="${member.image}" alt="${member.name}">
+        <div class="member-info">
+            <h3>${member.name}</h3>
+            <p>Address: ${member.address}</p>
+            <p>Phone:${member.phone}</p>
+            <p><strong>Category:</strong> ${member.category}</p>
+            <a href="${member.website}" target="_blank">Visit Website</a>
+        </div>
+        
+        `
+
+        memberSection.appendChild(card)
+    })
+
+}
+
+const filtereMembers=async()=>{
+    const keyword=searchInput.value.toLowerCase()
+    const categoryKeyword=searchCategory.value
+
+    const filtered= await members.filter(member=>{
+        const matchedKeyWord=member.name.toLowerCase().includes(keyword)||
+            member.category.toLowerCase().includes(keyword)||
+            member.address.toLowerCase().includes(keyword)
+
+        const matchedCategory= categoryKeyword==="all"||member.category===categoryKeyword
+
+        return matchedKeyWord && matchedCategory
+    })
+    displayMembers(filtered)
+}
+
+searchInput.addEventListener("keyup", filtereMembers)
+searchCategory.addEventListener("change",filtereMembers)
+getMembers()
+
