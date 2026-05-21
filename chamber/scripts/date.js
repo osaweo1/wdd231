d9c06d8fb89fbe85beb678a2aa75ba27ec064f40
osawe <osamudiamenosawe@gmail.com>
2026-05-21 12:23:24 +0100

    Delete chamber directory

--- chamber/scripts/date.js
+++
@@ -1,5 +0,0 @@
-const year = document.querySelector("#year");
-year.textContent = new Date().getFullYear();
-
-document.getElementById("lastModified").textContent =
-`Last Modified: ${document.lastModified}`;
